4db3bedc862c079cb751665fd1dc941a7a72c6aa
magnonta <magnonta@users.noreply.github.com>
2021-12-30 17:54:30 +0000

    deploy: f8f5b5757fad119d575acd4270ff102ebecf12b9

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
 * Start Bootstrap - Brutus Wear v0.0.1 (https://myusername.github.io)
-* Copyright 2013-2021 
-* Licensed under ISC (https://github.com/StartBootstrap/brutus/blob/master/LICENSE)
+* Copyright 2013-2021 undefined
+* Licensed under undefined (https://github.com/StartBootstrap/brutus/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
